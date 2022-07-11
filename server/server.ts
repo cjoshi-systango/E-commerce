7558e9bca4f0982ff9ce9d69202bd39e4b37a456
cjoshi-systango <cjoshi@isystango.com>
2022-07-11 19:40:18 +0530

    feat:created server for e-commerce

---
+++ server/server.ts
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv'
+dotenv.config();
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import http from "http"
+import userRouter from "../routes/user"
+import passport from "passport"
+import session from "express-session";
+import passportInitialize from '../config/passport-config'
+import sequelize from "../db/sequelizeConnection";
+import productRouter from '../routes/product';
+import cartRouter from "../routes/cart"
+
+const PORT = process.env.PORT||4002
+const app = express();
+sequelize.sync({alter:true});
+passportInitialize(passport);
+const MemoryStore = new session.MemoryStore();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret:`${process.env.SECRET}`,
+    resave: false,
+    saveUninitialized: false,
+    store: MemoryStore
+}))
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/e-commerce",userRouter)
+app.use("/e-commerce",productRouter)
+app.use("/e-commerce",cartRouter)
+
+
+
+const server = http.createServer(app);
+server.listen(PORT,()=>{
+    console.log("server listen " + PORT);
+    
+})
